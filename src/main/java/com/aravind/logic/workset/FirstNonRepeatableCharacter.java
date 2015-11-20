6ae2be46557c96beaca989c5853efedaffef5315
Aravind Cheekkallur <caravinden@hotmail.com>
2015-11-20 18:00:52 +0530

    comments added

--- src/main/java/com/aravind/logic/workset/FirstNonRepeatableCharacter.java
+++ src/main/java/com/aravind/logic/workset/FirstNonRepeatableCharacter.java
@@ -2,19 +2,28 @@ package com.aravind.logic.workset;
 
 import java.util.LinkedHashMap;
 
+/**
+ * For using this class we can find out first non repeatable character with
+ * different java approch
+ * 
+ * @author Aravind
+ *
+ */
 public final class FirstNonRepeatableCharacter {
 
 	public FirstNonRepeatableCharacter() {
-		
+
 	}
-	public final void getFirstNonRepeatableCharacter(String strValue){
+
+	public final void getFirstNonRepeatableCharacter(String strValue) {
+		/* For this approch using linked hashmap to find the charactor */
 		LinkedHashMap<Character, Integer> charMap = new LinkedHashMap<Character, Integer>();
-		for(char ch: strValue.toCharArray()){
-			charMap.put(ch, charMap.containsKey(ch)? charMap.get(ch)+1 : 1);
+		for (char ch : strValue.toCharArray()) {
+			charMap.put(ch, charMap.containsKey(ch) ? charMap.get(ch) + 1 : 1);
 		}
-		for(char ch: strValue.toCharArray()){
-			if(charMap.get(ch) == 1){
-				System.out.println("Non repearable charactor:: "+ch);
+		for (char ch : strValue.toCharArray()) {
+			if (charMap.get(ch) == 1) {
+				System.out.println("Non repearable charactor:: " + ch);
 				break;
 			}
 		}
